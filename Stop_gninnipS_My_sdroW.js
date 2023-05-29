8d9d1a68f362d8b82ba1ad8bbc92fdfe2d8f0171
NikitaGrimes <nikita.rick@gmail.com>
2023-05-29 19:24:40 +0300

    code fix

--- Stop_gninnipS_My_sdroW.js
+++ Stop_gninnipS_My_sdroW.js
@@ -1,11 +1,5 @@
 //https://www.codewars.com/kata/5264d2b162488dc400000001
 
 function spinWords(string){
-  let words = string.split(" ");
-  words.map((el, index, arr) => {
-    if(el.length >= 5){
-      arr[index] = [...el].reverse().join("");
-    }
-  })
-  return words.join(" ");
+  return string.replace(/\w{5,}/g, word => word.split("").reverse().join(""));
 }
